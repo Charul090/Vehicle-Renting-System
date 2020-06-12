8d2c7a4868e1d3c49aca98ad30a5c8bb95801dcb
Charul090 <baricharul9@hotmail.com>
2020-06-12 17:19:43 +0530

    BACKEND:User Login functionality

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,8 @@ from flask import request
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 import json
+import time
+import jwt
 
 app=Flask(__name__)
 
@@ -35,3 +37,59 @@ def userRegister():
     cur.close()
 
     return json.dumps({"message":"User Registration is successfully"})
+
+@app.route("/userlogin")
+def userLogin():
+    username=request.json["username"]
+    password=request.json["password"]
+
+    cur=mysql.connection.cursor()
+
+    cur.execute(''' SELECT username,password FROM user ''')
+    result=cur.fetchall()
+
+    flag=False
+
+    for x in result:
+        if x[0] == username:
+            if x[1] == password:
+                flag=True
+    
+    if flag:
+        payload={
+                    "username":username,
+                    "status":"logged-in",
+                    "time":time.time()+172800
+                }
+
+        key="masai"
+
+        encode=jwt.encode(payload,key)
+
+        return json.dumps({"token":encode.decode(),"message":"Login Successfull","error":False})
+    else:
+        return json.dumps({"message":"Login UnSuccessfull","error":True})
+
+
+@app.route("/auth_check")
+def authCheck():
+    token=request.json["token"]
+
+    key="masai"
+
+    data=jwt.decode(token,key)
+
+    current_time=time.time()
+
+    if data["time"] < current_time:
+        return json.dumps({
+            "error":True,
+            "message":"Invalid Token"
+        })
+    else:
+        return json.dumps({
+            "error":False,
+            "message":"Valid Token"
+        })
+
+    
